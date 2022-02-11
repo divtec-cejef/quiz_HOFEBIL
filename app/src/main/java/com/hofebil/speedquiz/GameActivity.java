adfa5e38a65bf2a85683ac0f6c104ec471ffc993
Bilel Hofer <bilel.hofer@divtec.ch>
2022-02-11 12:14:37 +0100

    activity activity_game.xml complete

--- app/src/main/java/com/hofebil/speedquiz/GameActivity.java
+++ app/src/main/java/com/hofebil/speedquiz/GameActivity.java
@@ -3,6 +3,8 @@ package com.hofebil.speedquiz;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatDelegate;
@@ -18,21 +20,32 @@ public class GameActivity extends Activity {
 
     private boolean darkMode;
 
+    private Button btVrai2;
+    private Button btVrai1;
+
+    private View finiLayout;
+
+    private Button btMenu;
+    private Button btAgain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        Intent intent = getIntent();
 
-
+        btMenu = findViewById(R.id.fini_cancel_bt);
+        btAgain = findViewById(R.id.fini_again_bt);
+        finiLayout = findViewById(R.id.fini_layout);
+        btVrai2 = findViewById(R.id.game_bt_vrai_player2);
+        btVrai1 = findViewById(R.id.game_bt_vrai_player1);
         player1 = findViewById(R.id.game_name_player1);
         player2 = findViewById(R.id.game_name_player2);
 
         // on récupère les valeur
-        name1 = intent.getStringExtra("player1");
-        name2 = intent.getStringExtra("player2");
-        nombreSecondeQuestion = intent.getExtras().getInt("nombreSecondeQuestion");
-        darkMode = intent.getExtras().getBoolean("darkMode");
+        name1 = getIntent().getStringExtra("player1");
+        name2 = getIntent().getStringExtra("player2");
+        nombreSecondeQuestion = getIntent().getExtras().getInt("nombreSecondeQuestion");
+        darkMode = getIntent().getExtras().getBoolean("darkMode");
 
         player2.setText(name2);
         player1.setText(name1);
@@ -42,5 +55,35 @@ public class GameActivity extends Activity {
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // bt test menu fin de jeu
+        btVrai2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finiLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // bt retour au menu
+        btMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        // bt retour au jeu
+        btMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finiLayout.setVisibility(View.GONE);
+            }
+        });
     }
 }
